ed5794c7457fb7d6ed966b4bf35d770b9fa10fd5
Matt Van Horn <mattvanhorn@gmail.com>
2016-10-03 16:17:11 -0700

    Changes for Heroku Deployment

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mattvanhorn, Mattvanhorn.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "warm-earth-79570.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,6 +59,14 @@ config :logger, level: :info
 #     config :mattvanhorn, Mattvanhorn.Endpoint, server: true
 #
 
+# Configure your database
+config :mattvanhorn, Mattvanhorn.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
